4fb327372ac5ab71c8f946dfc0988f7fc81cafea
4z4c <arturoyz2105@gmail.com>
2025-02-15 21:53:03 -0500

    15

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,26 @@
-// import { WidgetItem } from "@/components";
+import { WidgetItem } from "@/components";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+
+  const sesion = await getServerSession();
+
+  if( !sesion ) {
+    redirect('/api/auth/signin');
+  }
+
   return (
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+    <div className="grid gap-6 grid-cols-1 md:grid-cols-2">
 
       {/* TODO: src/components <WidgetItem /> */}
-      {/* <WidgetItem /> */}
+      <WidgetItem title="Usuario desde S-Side">
+        <div className="flex flex-col">
+          <span>{ sesion.user?.name}</span>
+          <span>{ sesion.user?.image}</span>
+          <span>{ sesion.user?.email}</span>
+        </div>
+      </WidgetItem>
       {/* TODO: Fin <WidgetItem /> */}
 
     </div>
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image"
 import Link from "next/link"
-import { CiBookmarkCheck, CiLogout } from "react-icons/ci"
+import { CiLogout } from "react-icons/ci"
 import { SidebarItem } from "./SidebarItem"
 import { IoBasketOutline, IoCalendarOutline, IoCheckboxOutline, IoCodeWorkingOutline, IoListOutline } from "react-icons/io5"
+import { getServerSession } from "next-auth"
+import { authOptions } from "@/app/api/auth/[...nextauth]/route"
 
 const menuItems = [
   {
@@ -32,7 +34,16 @@ const menuItems = [
   },
 ]
 
-export const SideBar = () => {
+export const SideBar = async() => {
+
+  const sesion = await getServerSession(authOptions);
+
+  const avatarUrl = ( sesion?.user?.image )
+    ? sesion.user.image
+    : "https://st2.depositphotos.com/3258807/7830/i/450/depositphotos_78307434-stock-photo-close-up-of-smiling-man.jpg"
+
+  const userNmae = sesion?.user?.name ?? 'No Name';
+
   return (
     <aside className="ml-[-100%] fixed z-10 top-0 pb-3 px-6 w-full flex flex-col justify-between h-screen border-r bg-white transition duration-300 md:w-4/12 lg:ml-0 lg:w-[25%] xl:w-[20%] 2xl:w-[15%]">
       <div>
@@ -54,14 +65,14 @@ export const SideBar = () => {
         <div className="mt-8 text-center">
           {/* Next/Image */}
           <Image 
-            src="https://st2.depositphotos.com/3258807/7830/i/450/depositphotos_78307434-stock-photo-close-up-of-smiling-man.jpg" 
+            src={ avatarUrl }
             alt="" 
             width={ 150 }
             height={ 150 }
             className="w-10 h-10 m-auto rounded-full object-cover lg:w-28 lg:h-28" 
             priority={ true }
           />
-          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">Cynthia J. Watts</h5>
+          <h5 className="hidden mt-4 text-xl font-semibold text-gray-600 lg:block">{ userNmae }</h5>
           <span className="hidden text-gray-400 lg:block">Admin</span>
         </div>
 
